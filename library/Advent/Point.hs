31c810559d068c9c48227218743482ee0ded3d15
Chris Parks <christopher.daniel.parks@gmail.com>
2019-12-10 15:20:54 -0800

    More concise Show instance for Point

--- library/Advent/Point.hs
+++ library/Advent/Point.hs
@@ -11,13 +11,18 @@ import Advent.Prelude
 
 import GHC.Generics (Generic)
 import Lens.Micro
+import Text.Show
 
 data Point = Point
   { _x :: {-# UNPACK #-} Int
   , _y :: {-# UNPACK #-} Int
   }
-  deriving stock (Eq, Ord, Show, Generic)
+  deriving stock (Eq, Ord, Generic)
   deriving anyclass (Hashable)
+
+instance Show Point where
+  showsPrec _ Point {..} =
+    showChar '(' . shows _x . showString ", " . shows _y . showChar ')'
 
 x :: Lens' Point Int
 x = lens _x $ \p v -> p { _x = v }
