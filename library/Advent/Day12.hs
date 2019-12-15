866d79e2c2fb8a744957f6d5a42a4fb1ab61e53e
Christopher Parks <christopher.daniel.parks@gmail.com>
2019-12-15 12:01:21 -0800

    Add solution for day 12
    This was the first one I needed some help on. Simulating and storing
    every step takes too much memory. A few folks on the advent-of-code
    subreddit hinted that we should use least-common-multiple. From
    Wikipedia:
    
    > Suppose there are three planets revolving around a star which take l,
    > m and n units of time respectively to complete their orbits. Assume
    > that l, m and n are integers. Assuming the planets started moving
    > around the star after an initial linear alignment, all the planets
    > attain a linear alignment again after LCM(l, m, n) units of time
    
    We find the step where velocity returns to zero and then use the
    calculation above to find the next time it will happen again.
    
    See https://en.wikipedia.org/wiki/Least_common_multiple#Planetary_alignment

--- library/Advent/Day12.hs
+++ library/Advent/Day12.hs
@@ -9,17 +9,17 @@ import Advent.Text (readCommaSep)
 import Advent.Vec3 (Vec3(..))
 import qualified Advent.Vec3 as Vec3
 import Data.List ((!!))
+import Data.Maybe (fromJust)
 import Data.Text.IO (getContents)
 import Lens.Micro
 
 main :: Part -> IO ()
 main part = do
   moons <- parse <$> getContents
+  let steps = iterate simulate $ Body.static <$> moons
   case part of
-    Part1 -> do
-      let steps = iterate simulate $ Body.static <$> moons
-      print $ sum $ totalEnergy <$> steps !! 1000
-    Part2 -> putStrLn "not implemented"
+    Part1 -> print $ sum $ totalEnergy <$> steps !! 1000
+    Part2 -> print $ fromJust $ duplicate steps
 
 parse :: Text -> [Vec3 Int]
 parse = mapMaybe parseVec3 . lines
@@ -47,3 +47,18 @@ attract moon other = moon & Body.vel +~ signum (other - moon ^. Body.pos)
 
 applyVelocity :: [Moon] -> [Moon]
 applyVelocity = map Body.move
+
+duplicate :: [[Moon]] -> Maybe Integer
+duplicate steps = do
+  l <- stepsToZero Vec3.x
+  m <- stepsToZero Vec3.y
+  n <- stepsToZero Vec3.z
+  pure $ 2 * (lcm l m `lcm` lcm m n)
+ where
+  stepsToZero :: Lens' (Vec3 Int) Int -> Maybe Integer
+  stepsToZero axis = fst <$> find ((== 0) . velocity1D axis . snd) labeled
+
+  labeled = drop 1 $ zip [0 ..] steps
+
+velocity1D :: Lens' (Vec3 Int) Int -> [Moon] -> Int
+velocity1D axis = sum . map (\moon -> abs $ moon ^. Body.vel . axis)
